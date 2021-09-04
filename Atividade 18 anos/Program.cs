6a902d1115e69ab8be397e25c6202312c122c2ae
André Savio Canova Motter <am0125355@>
2021-09-04 09:23:03 -0300

    first commit

---
+++ Atividade 18 anos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Atividade_18_anos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe sua idade:");
+            recebeidade();
+        }
+
+        static void recebeidade()
+        {
+            int idade = Convert.ToInt32(Console.ReadLine());
+            string resposta = calculaIF(idade);
+            Console.WriteLine(resposta);
+        }
+
+        static string calculaIF(int idade)
+
+        {
+            if (idade >= 17 && idade < 30)
+            { 
+                return "Adulto";
+            }
+            else if (idade >= 30 && idade < 50)
+            {
+                return "Maia idade";
+            }
+            else if (idade >= 50 && idade < 70)
+            {
+                return "Idoso";
+            }
+            else if (idade < 18)
+            {
+                return "Jovem";
+            }
+            else
+            {
+                return "Está fazendo hora extra";
+            }
+            
+        }
+    }
+}
